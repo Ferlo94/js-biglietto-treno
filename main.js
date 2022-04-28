f137eb21d29ef15497880eb7e658c02d5dfed2d8
Ferlo94 <laferlart@gmail.com>
2022-04-28 16:22:29 +0200

    prove sconto

---
+++ main.js
@@ -0,0 +1,23 @@
+
+// esercizio di oggi: calcolo del prezzo del biglietto del treno
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, 
+// per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+
+// Quanti chilometri devi percorrere?
+// Quanti anni hai?
+
+const chilometriUtente = Number(prompt("Inserisci i chilometri che vuoi percorrere"));
+const etaUtente = Number(prompt("Inserisci la tua eta'"));
+
+let costoBiglietto = chilometriUtente * 0.21;
+
+
+if (etaUtente < 18) {
+    costoBiglietto = costoBiglietto * 20 / 100;
+}
